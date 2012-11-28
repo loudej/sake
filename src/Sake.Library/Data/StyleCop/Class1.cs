5609bd4e92b0fdb2cb79538a005ba1df931cf2aa
Louis DeJardin <louis.dejardin@gmail.com>
2012-11-27 17:04:15 -0800

    Adding stylecop tag

---
+++ src/Sake.Library/Data/StyleCop/Class1.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="Class1.cs" company="Madness"> hello </copyright>
+
+namespace Testing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Most frequently deleted class in history
+    /// </summary>
+    public class Class1
+    {
+    }
+}
